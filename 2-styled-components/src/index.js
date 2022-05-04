f0aee74da5290fc776be2bb641855addee4fc1a9
Firdous M <firdous.m@litmus7.com>
2022-05-04 14:17:42 +0530

    Assignment 2- Styled Components completed

--- 2-styled-components/src/index.js
+++ 2-styled-components/src/index.js
@@ -1,9 +1,28 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import styled from 'styled-components';
 
 const App = () => {
-  return <h1>Hello React!</h1>;
+  const Header = styled.h1`
+    color: red;
+
+    @media (max-width: 480px){
+      font-size: 14px;
+      color: green;
+    }
+  `;
+ const Hero = styled(Header)`
+    font-size: 100px;
+    color: blue;
+  `;
+
+  return (
+  <>
+  <Hero>Iam hero</Hero>
+  <Header>Hello React</Header>
+  </>);
 };
+
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
